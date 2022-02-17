ab1ef7f5864dc4edd55bd4bcd1021d7bc5dd1c6e
Ender <33337309+e3ndr@users.noreply.github.com>
2022-02-16 22:25:37 -0600

    All the generation logic.

---
+++ downloadAndInstall.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+
+# Full URL Format:
+# https://mirror.umd.edu/eclipse/eclipse/downloads/drops4/R-4.22-202111241800/swt-4.22-win32-win32-x86_64.zip
+
+
+MIRROR='https://mirror.umd.edu/eclipse/eclipse/downloads/drops4/'
+
+IFS=$'\n'
+to_download=`grep "^[^#;]" urls.txt`
+
+for item in $to_download;
+do
+    filename=`echo $item | cut -d "/" -f2`
+    itemName=${filename::${#filename}-4}
+
+    echo ------------------------
+
+    if [ ! -d cache/$itemName ] 
+    then
+        echo Downloading $filename to $itemName
+
+        mkdir cache/$itemName
+        wget -O cache/$filename $MIRROR$item
+        unzip -d cache/$itemName cache/$filename
+        rm cache/$filename
+    fi
+
+    version=`echo $itemName | cut -d "-" -f2`
+    artifactId=`echo $itemName | cut -d "-" -f3`.`echo $itemName | cut -d "-" -f4`.`echo $itemName | cut -d "-" -f5`
+
+    echo Installing $artifactId:$version
+
+    sh install.sh "cache/$itemName/swt.jar" "cache/$itemName/src.zip" $artifactId $version 
+done;
---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+FILE=$1
+SRC=$2
+ARTIFACT_ID=$3
+VERSION=$4
+
+DIR=`pwd`/maven
+
+mvn deploy:deploy-file \
+		-Durl=file://$DIR \
+		-Dsources=$SRC \
+		-Dfile=$FILE \
+		-DgroupId=org.eclipse.swt \
+		-DartifactId=$ARTIFACT_ID \
+		-Dversion=$VERSION \
+        -Dpackaging=jar \
+        -DgeneratePom=true
